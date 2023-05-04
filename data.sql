db4e0f6c28ed4d9aacaea2d159fc7fd890a8b060
Kevin Okoth <kevinokotth04@gmail.com>
2023-05-04 14:12:30 +0300

    Insert data into the species table

--- data.sql
+++ data.sql
@@ -32,3 +32,7 @@ INSERT INTO owners (full_name, age) VALUES('Bob ', 45);
 INSERT INTO owners (full_name, age) VALUES('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES('Jodie Whittaker', 38);
+
+-- Insert data into the species table
+INSERT INTO species (name)
+ VALUES ('Pokemon'), ('Digimon');
