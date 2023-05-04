5ce14115c347e5ec4d93382884759de3bc627776
Kevin Okoth <kevinokotth04@gmail.com>
2023-05-04 14:10:48 +0300

    Insert data into the owners table

--- data.sql
+++ data.sql
@@ -24,3 +24,11 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 values ('Blossom','1998-10-13',3,TRUE,17.00);
  INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 values ('Ditto','2022-05-14',4,TRUE,22.00);
+
+-- Insert data into the owners table
+INSERT INTO owners (full_name, age) VALUES('Sam Smith', 34); 
+INSERT INTO owners (full_name, age) VALUES('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES('Bob ', 45);
+INSERT INTO owners (full_name, age) VALUES('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES('Jodie Whittaker', 38);
